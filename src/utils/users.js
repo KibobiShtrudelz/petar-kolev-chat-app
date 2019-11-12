f4bf135cc7158b2e3febd02e3ab17156d335db23
k0l3v <petarko@tryond.com>
2019-11-12 15:58:01 +0200

    168. Storing Users: Part I

---
+++ src/utils/users.js
@@ -0,0 +1,52 @@
+const users = [];
+
+// addUser, removeUser, getUser, getUsersInRoom
+
+const addUser = ({ id, username, room }) => {
+  // Clean the data
+  username = username.trim().toLowerCase();
+  room = room.trim().toLowerCase();
+
+  // Validate the data
+  if (!username || !room) {
+    return {
+      error: "Username and room are required!"
+    };
+  }
+
+  // Check for existing user
+  const existingUser = users.find(user => {
+    return user.room === room && user.username === username;
+  });
+
+  // Validate username
+  if (existingUser) {
+    return {
+      error: "Username is taken!"
+    };
+  }
+
+  // Store user
+  const user = { id, username, room };
+  users.push(user);
+
+  return { user };
+};
+
+const removeUser = id => {
+  const index = users.findIndex(user => user.id === id);
+
+  if (index !== -1) {
+    return users.splice(index, 0)[0];
+  }
+};
+
+addUser({ id: 22, username: "Peckata", room: "Sofia" });
+console.log(users);
+
+const removedUser = removeUser(22);
+console.log(removedUser);
+
+const getUser = () => {};
+
+const getUsersInRoom = () => {};
