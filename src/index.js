21a243b292b37fbaa9542a7636608149205e8b52
k0l3v <petarko@tryond.com>
2019-11-12 12:37:04 +0200

    163. Working with Time

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ const express = require("express");
 const socketio = require("socket.io");
 const Filter = require("bad-words");
 
+const { generateMessage } = require("./utils/messages");
+
 const app = express();
 // we set the "server" var manually here instead of express behind the scenes because we need
 // it in order to give it to socket.io and connect to it
@@ -21,8 +23,8 @@ app.use(express.static(publicDirectoryPath));
 io.on("connection", socket => {
   console.log("New WebSocket connection!");
 
-  socket.emit("message", "Welcome!");
-  socket.broadcast.emit("message", "A new user has joined!");
+  socket.emit("message", generateMessage("Welcome"));
+  socket.broadcast.emit("message", generateMessage("A new user has joined!"));
 
   socket.on("sendMessage", (message, callback) => {
     const filter = new Filter();
@@ -31,7 +33,7 @@ io.on("connection", socket => {
       return callback("Profanity is not allowed!");
     }
 
-    io.emit("message", message);
+    io.emit("message", generateMessage(message));
     callback();
   });
 
@@ -45,7 +47,7 @@ io.on("connection", socket => {
 
   // when user disconnects we use "socket.on" inside the "io.on" event
   socket.on("disconnect", () => {
-    io.emit("message", "A user has left");
+    io.emit("message", generateMessage("A user has left"));
   });
 });
 
